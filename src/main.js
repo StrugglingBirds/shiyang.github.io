afa9d3f08ec61809a667e58e3b9ae63b59f14588
shiy-s@glodon.com <shiy-s@glodon.com>
2022-01-28 17:35:35 +0800

    feat: 增加代码高亮

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import { useVant } from './components/vant'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import 'vant/lib/index.css'
+import './assets/css/initial.css'
+import './assets/css/common.css'
+
+const vm = createApp(App)
+vm.use(router)
+useVant(vm)
+vm.mount('#app')
